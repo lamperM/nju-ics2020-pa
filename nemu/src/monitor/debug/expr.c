28de98302d2b2c94be1f571e90b02511448bbe05
tracer-ics2020 <tracer@njuics.org>
2022-04-17 12:16:46 +0800

    >  compile 202122280701    Static hostname: wanglu-hp    Pretty hostname: wanglu-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 057fcf0daf8d4f66bd4d3d058e8d8c73            Boot ID: 28bae92152bc45718105d46e8a682565   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-40-generic       Architecture: x86-64  12:16:46 up 1 day, 21:40,  3 users,  load average: 0.35, 0.29, 0.21

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -68,7 +68,7 @@ void init_regex() {
     }
   }
 
-  //test_expr();
+  test_expr();
 }
 
 typedef struct token {
@@ -195,19 +195,19 @@ bool priority_is_higher(int op1, int op2) {
 }
 
     
-uint32_t eval(int p, int q) {
+word_t eval(int p, int q) {
     if (p > q) {
         printf("p = %d, q = %d(p > q), error\n", p, q);
         assert(0);
     } else if (p == q) {
-        return (uint32_t)strtol(tokens[p].str, NULL, 10); // only base-10 supported
+        return (word_t)strtol(tokens[p].str, NULL, 10); // only base-10 supported
     } else if (check_parentheses(p, q) == true) {
         return eval(p + 1, q - 1);
     } else {
         /* find 主运算符 */
         int main_op_pos = 0;
         int main_op = TK_INVAILD; // poison value
-        uint32_t val1, val2;
+        word_t val1, val2;
 
 
         for (int i = q, need_brkt = 0; i >= p; i--) {
@@ -250,7 +250,7 @@ uint32_t eval(int p, int q) {
 
 }  
 
-uint32_t expr(char *e, bool *success) {  
+word_t expr(char *e, bool *success) {  
     if (!make_token(e)) {
         *success = false;
         return 0;
@@ -258,7 +258,7 @@ uint32_t expr(char *e, bool *success) {
 
     /* TODO: Insert codes to evaluate the expression. */
     int p, q;
-    uint32_t res;
+    word_t res;
 
     p = 0;
     q = nr_token - 1;
