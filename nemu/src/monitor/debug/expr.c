8614d0aa3ea5ae229d17e2732bb502f8045d3599
tracer-ics2020 <tracer@njuics.org>
2022-04-18 21:53:00 +0800

    >  compile 202122280701    Static hostname: wanglu-hp    Pretty hostname: wanglu-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 057fcf0daf8d4f66bd4d3d058e8d8c73            Boot ID: c8f87a85c798499e9810ec09feb83b95   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-40-generic       Architecture: x86-64  21:53:00 up  2:57,  2 users,  load average: 0.10, 0.19, 0.10

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -47,8 +47,8 @@ static struct rule {
   {"&&", TK_AND},       // and
   {"$", TK_DEREF},      // register name prefix
   /* operation token end */
-  {"[0-9]+U?", TK_NUM},   // dec number (also '[[:digit:]]+U?' in POSIX)
   {"0[xX][0-9]+U?", TK_HEX_NUM}, 
+  {"[0-9]+U?", TK_NUM}, // dec number (also '[[:digit:]]+U?' in POSIX)
 
 };
 
