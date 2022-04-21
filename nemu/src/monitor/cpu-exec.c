89f45985a925a73c46e41b15d9310a8907c4afd0
tracer-ics2020 <tracer@njuics.org>
2022-04-21 20:45:29 +0800

    >  compile 202122280701    Static hostname: wanglu-hp    Pretty hostname: wanglu-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 057fcf0daf8d4f66bd4d3d058e8d8c73            Boot ID: c8f87a85c798499e9810ec09feb83b95   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-40-generic       Architecture: x86-64  20:45:29 up 3 days,  1:49,  1 user,  load average: 0.07, 0.08, 0.04

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -109,7 +109,7 @@ void cpu_exec(uint64_t n) {
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
     case NEMU_STOP:
-
+        break;
     case NEMU_END: case NEMU_ABORT:
       Log("nemu: %s\33[0m at pc = " FMT_WORD "\n\n",
           (nemu_state.state == NEMU_ABORT ? "\33[1;31mABORT" :
