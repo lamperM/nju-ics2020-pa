a789eb1d2adf842c27f42b02e01d7afa3984a99d
tracer-ics2020 <tracer@njuics.org>
2022-06-16 18:50:11 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 9b6cf3cdd45a4702a0b53f08904bd848   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-48-generic       Architecture: x86-64  18:50:11 up  3:04,  2 users,  load average: 0.14, 0.05, 0.01

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -34,7 +34,6 @@ static inline def_rtl(push, const rtlreg_t* src1) {
 static inline def_rtl(pop, rtlreg_t* dest) {
   // dest <- M[esp]
   rtl_lm(s, dest, &(cpu.esp), 0, s->dest.width);
-  printf("get esp data: %x\n", *dest);
   // esp <- esp + 4
   cpu.esp += 4;
 }
