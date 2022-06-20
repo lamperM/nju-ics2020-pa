46dcec2622ccff16cea61bd5b74621809bb4d079
tracer-ics2020 <tracer@njuics.org>
2022-06-20 20:34:14 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 9b6cf3cdd45a4702a0b53f08904bd848   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-48-generic       Architecture: x86-64  20:34:15 up 4 days,  4:48,  3 users,  load average: 0.10, 0.02, 0.01

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -13,7 +13,7 @@ static inline void set_width(DecodeExecState *s, int width) {
 /* 0x80, 0x81, 0x83 */
 static inline def_EHelper(gp1) {
   //  printf("Operand dest.type: %d, r:%d\n", s->dest.type, s->dest.reg);
-    printf("ext_code: %d\n", s->isa.ext_opcode);
+  //printf("ext_code: %d\n", s->isa.ext_opcode);
   //  printf("Operand src1.type: %d, val: %d\n", s->src1.type, s->src1.val);
   switch (s->isa.ext_opcode) {
       case 4: 
@@ -58,6 +58,7 @@ static inline def_EHelper(gp4) {
 
 /* 0xff */
 static inline def_EHelper(gp5) {
+    printf("s.isa.moff:%d\n", s->isa.moff);
   switch (s->isa.ext_opcode) {
     EMPTY(0) EMPTY(1) EMPTY(2) EMPTY(3)
     EMPTY(4) EMPTY(5) EMPTY(6) EMPTY(7)
