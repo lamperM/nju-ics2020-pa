a5846681ab686596550a38946cae52f1fa4461f4
tracer-ics2020 <tracer@njuics.org>
2022-07-05 21:00:01 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 7a2cff88bfb94e328f016d003306d772   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  21:00:01 up 11:33,  2 users,  load average: 0.24, 0.12, 0.15

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -30,7 +30,7 @@ static inline def_EHelper(gp1) {
           /* Acutally, CMP instruction do NOT care the result of sub,
            * but only update the flags. So this may be a bug. */
           rtl_subi(s, ddest, ddest, s->src1.simm);
-          rtl_update_ZFSF(s, ddest, s->width); 
+          //rtl_update_ZFSF(s, ddest, s->width); 
           break;
           
 
