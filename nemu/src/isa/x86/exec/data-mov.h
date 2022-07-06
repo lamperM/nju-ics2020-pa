231dcda1bec186673533bbe979ce51f9ddc79d6b
tracer-ics2020 <tracer@njuics.org>
2022-07-07 00:33:52 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 7a2cff88bfb94e328f016d003306d772   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  00:33:53 up 1 day, 15:07,  1 user,  load average: 1.13, 1.03, 0.77

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -55,7 +55,9 @@ static inline def_EHelper(cwtl) {
 }
 
 static inline def_EHelper(movsx) {
+    printf("before dest->width: %d\n", id_dest->width);
   id_dest->width = s->isa.is_operand_size_16 ? 2 : 4;
+    printf("after dest->width: %d\n", id_dest->width);
   rtl_sext(s, ddest, dsrc1, id_src1->width);
   operand_write(s, id_dest, ddest);
   print_asm_template2(movsx);
