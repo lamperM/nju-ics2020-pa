e56373ec1b6718f4cf7a4d47cbbc6364a6f1a683
tracer-ics2020 <tracer@njuics.org>
2022-07-07 23:53:42 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 7a2cff88bfb94e328f016d003306d772   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  23:53:42 up 2 days, 14:27,  1 user,  load average: 0.54, 0.37, 0.28

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -14,7 +14,7 @@ static inline def_EHelper(sub) {
 static inline def_EHelper(cmp) {
   /* Acutally, CMP instruction do NOT care the result of sub,
    * but only update the flags. So this may be a bug. */
-  rtl_subi(s, ddest, ddest, s->src1.simm);
+  rtl_sub(s, ddest, ddest, dsrc1);
   printf("ddest = %d\n", *ddest);
   rtl_update_ZFSF(s, ddest, s->dest.width); 
   print_asm_template2(cmp);
