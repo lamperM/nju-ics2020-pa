a7f2a1dfc9fb33a57daf5d7fc9f04000cfc49b1a
tracer-ics2020 <tracer@njuics.org>
2022-07-07 20:53:52 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 7a2cff88bfb94e328f016d003306d772   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  20:53:52 up 2 days, 11:27,  2 users,  load average: 0.25, 0.33, 0.49

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -57,6 +57,7 @@ static inline def_DopHelper(SI) {
    operand_imm(s, op, load_val, ???, op->width);
    */
     sword_t simm = instr_fetch(&s->seq_pc, op->width);
+    printf("simm = %d\n", simm);
     operand_imm(s, op, load_val, simm, op->width);
 }
 
