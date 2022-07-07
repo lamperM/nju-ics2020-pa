ade7088dc348863e0c64eaad2f757b91cb8d5b15
tracer-ics2020 <tracer@njuics.org>
2022-07-07 21:14:03 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 7a2cff88bfb94e328f016d003306d772   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  21:14:03 up 2 days, 11:47,  2 users,  load average: 0.78, 1.27, 0.89

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -60,7 +60,7 @@ static inline def_DopHelper(SI) {
 
     if (op->width == 1) 
         operand_imm(s, op, load_val, (int8_t)simm, op->width);
-    else 
+    else if (op->width == 4) 
         operand_imm(s, op, load_val, (int32_t)simm, op->width);
 }
 
@@ -270,7 +270,7 @@ static inline def_DHelper(J) {
   decode_op_SI(s, id_dest, false);
   // the target address can be computed in the decode stage
   s->jmp_pc = id_dest->simm + s->seq_pc;
-  printf("s->seq_pc = %x\n", s->seq_pc);
+  printf("s->seq_pc = %x\n", s->jmp_pc);
 }
 
 static inline def_DHelper(push_SI) {
