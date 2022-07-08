eec75d3d9876dbc1a43ebfe81c2242b581ebe3b3
tracer-ics2020 <tracer@njuics.org>
2022-07-08 23:53:51 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 7a2cff88bfb94e328f016d003306d772   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  23:53:51 up 3 days, 14:27,  1 user,  load average: 0.00, 0.17, 0.18

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -19,12 +19,13 @@ static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  TODO();
+  *dest = ~(*src1);
 }
 
 static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- -src1
-  TODO();
+  // add by wanglu 7.8
+  *dest = -1 * (*src1);
 }
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
