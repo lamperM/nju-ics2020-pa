cc568b9ccf7b7db5804565bfb3a2ad35a59ff756
tracer-ics2020 <tracer@njuics.org>
2022-07-08 23:46:30 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 7a2cff88bfb94e328f016d003306d772   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  23:46:31 up 3 days, 14:20,  1 user,  load average: 0.37, 0.27, 0.16

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -95,6 +95,7 @@ static inline def_EHelper(2byte_esc) {
   /* TODO: Add more instructions!!! */
     IDEX (0x01, gp7_E, gp7)
     IDEX (0x94, setcc_E, setcc)
+    IDEX (0x95, setcc_E, setcc)
     IDEX (0xb6, mov_E2G, movzx) // 不能打印al 和 eax, 因为 set_width() 中统一设置的 
     IDEX (0xbe, mov_E2G, movsx); // add by wanglu 7.7
     default: exec_inv(s);
