a39e6eabba25fef636ff46f60b19000274ccf358
tracer-ics2020 <tracer@njuics.org>
2022-07-09 00:04:20 +0800

    >  compile 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 7a2cff88bfb94e328f016d003306d772   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  00:04:20 up 3 days, 14:37,  1 user,  load average: 0.12, 0.20, 0.18

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -134,6 +134,9 @@ again:
     IDEX (0x59, r, pop)  /* pop(ECX) */
     IDEX (0x5a, r, pop)  /* pop(EDX) */
     IDEX (0x5b, r, pop)  /* pop(EBX) */
+    IDEX (0x5c, r, pop)  /* pop(ESP) */
+    IDEX (0x5d, r, pop)  /* pop(EBP) */
+    IDEX (0x5e, r, pop)  /* pop(ESI) */
     IDEX (0x68, I, push) /* push(imm32) */
     IDEXW(0x6a, I, push, 1) /* push(imm8) */
     IDEXW(0x74, J, jcc, 1) /* jcc */
