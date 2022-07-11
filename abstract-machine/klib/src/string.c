b3ba75ce77f3c296993a613ce5d931bac7b9e806
tracer-ics2020 <tracer@njuics.org>
2022-07-11 23:57:39 +0800

    >  run 202122280701    Static hostname: soben-HP          Icon name: computer-laptop            Chassis: laptop         Machine ID: 401150cdfeea434599ffb173da63e071            Boot ID: 605e4b0f1f7a41ffa5f6b51956e1242c   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  23:57:40 up  2:21,  1 user,  load average: 0.39, 0.44, 0.44

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -34,7 +34,7 @@ char* strcat(char* dst, const char* src) {
     for (i = 0; src[i] != '\0'; i++) {
         dst[dst_len + i] = src[i];
     }
-    dst[i] = '\0';
+    dst[dst_len + i] = '\0';
     
   return dst;
 }
